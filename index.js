d8dcd6caaf3befa662f45f68bc9dca2f2dc94fed
Misha Huziuk <mihagu36@gmail.com>
2024-10-04 12:12:22 -0400

    publish package

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { add } = require('math-operations');
-const { print } = require('io');
+const { add } = require('@misha-huziuk/math-operations');
+const { print } = require('@misha-huziuk/io');
 
 print(`2 + 3: ${add(2, 3)}`);
