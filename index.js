a1d470ab6ac4f0144f6d7754677d45ea9af6b82b
Misha Huziuk <mihagu36@gmail.com>
2025-03-21 17:17:38 -0400

    rename packages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { add } = require('math-operations');
-const { print } = require('io');
+const { add } = require('@mishahusiuk/math-operations');
+const { print } = require('@mishahusiuk/io');
 
 print(`2 + 3: ${add(2, 3)}`);
